450693c28873fa275a781cd714102e00ad6ca625
Jennifer Moundanos <jmoundanos@gmail.com>
2020-04-23 21:04:14 -0500

    fixed image links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 
 const Header = props => (
     <div className="container">
-        <div className="title"><img alt="Stranger Things Logo" src={'/assets/images/logo.png'}></img></div>
+        <div className="title"><img alt="Stranger Things Logo" src={'assets/images/logo.png'}></img></div>
         <div className="message"><h4>Click an image to get a point, but don't click an image more than once!</h4></div>
         <div className="score"><h5>Score: {props.score}</h5></div>
         <div className="topScore"><h5>Top Score: {props.topScore}</h5></div>
