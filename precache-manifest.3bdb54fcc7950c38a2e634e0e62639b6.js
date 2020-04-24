17df582f3494818fbe6a67e71ed8829334ff5027
Jennifer Moundanos <jmoundanos@gmail.com>
2020-04-23 20:56:52 -0500

    Updates

---
+++ precache-manifest.3bdb54fcc7950c38a2e634e0e62639b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80530845ce871f3075c31c6e6170d4c1",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "6e17a043902fb4096139",
+    "url": "/clicky-game/static/css/main.f8fddc4e.chunk.css"
+  },
+  {
+    "revision": "2b418907297a29c6c744",
+    "url": "/clicky-game/static/js/2.2936a901.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.2936a901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e17a043902fb4096139",
+    "url": "/clicky-game/static/js/main.323cdca8.chunk.js"
+  },
+  {
+    "revision": "9025a9c5d0326b1623d9",
+    "url": "/clicky-game/static/js/runtime-main.ec6097ba.js"
+  }
+]);
